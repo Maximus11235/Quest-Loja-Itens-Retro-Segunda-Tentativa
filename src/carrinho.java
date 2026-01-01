8bdd4d0a43b45478a4e6c7be333e02db67974f3f
Maximus Rosa <maximusrosadonascimento@gmail.com>
2026-01-01 17:06:20 -0300

    removendo arquivos .class do controle de versão

--- src/carrinho.java
+++ src/carrinho.java
@@ -1,6 +1,26 @@
-import javax.swing.JFrame;
-import java.util.ArrayList;;
-public class carrinho extends JFrame{
-    ArrayList<itemVenda> carrinho= new ArrayList<>();
-    
+import java.util.ArrayList;
+
+public class carrinho {
+    private ArrayList<itemVenda> carrinho= new ArrayList<>();
+    private double valorTotal;
+    private double valorDesconto=0.85;
+    private double valorComDesconto;
+
+    public double getValorComDesconto() {
+        return valorComDesconto;
+    }
+    public void setValorComDesconto(){
+        valorComDesconto= getValortotal()*valorDesconto;
+    }
+    public void setValorTotal() {
+        this.valorTotal = getValortotal();
+    }
+    public double getValortotal(){
+        valorTotal=0;
+        for(itemVenda item : carrinho){
+            valorTotal+=item.getPreco();
+        }
+        return valorTotal;
+    }
+    public void converter(String )
 }
--- src/itemVenda.java
+++ src/itemVenda.java
@@ -1,6 +1,37 @@
-import javax.swing.JFrame;
 
-public class itemVenda extends JFrame{
+
+public class itemVenda{
+    private String nome;
+    private String preco;
+    private String imagempath;
+
+    public itemVenda(String nome, String preco, String imagempath) {
+        this.nome = nome;
+        this.preco = preco;
+        this.imagempath = imagempath;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getPreco() {
+        return preco;
+    }
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+    public String getImagempath() {
+        return imagempath;
+    }
+    public void setImagempath(String imagempath) {
+        this.imagempath = imagempath;
+    }
     
     
+
+    
+
 }
